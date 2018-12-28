75df621edc916b2cd512f5850bc140a4a8d02016
siva <siva@neseapl.com>
2018-12-28 15:24:45 +0800

    InputView Update

--- MessengerKit/Input/MSGInputView.swift
+++ MessengerKit/Input/MSGInputView.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+enum MessageType {
+    case Text
+    case AudioRecordStart
+    case AudioRecordEnd
+}
+
 /// The base class for the input view used within `MSGMessengerViewController`.
 /// This can be subclassed to add custom views.
 open class MSGInputView: UIControl {
@@ -18,10 +24,22 @@ open class MSGInputView: UIControl {
     /// The send button
     @IBOutlet public var sendButton: UIButton!
 
+    /// The send button
+    @IBOutlet public var audioButton: UIButton!
+
+    /// The send button
+    @IBOutlet public var attachButton: UIButton!
+    
+    @IBOutlet public var audioView: UIView!
+
+    var messageType:MessageType = MessageType.Text
+    
     /// The height constraint to be modified as required.
     /// This should not be set manually but instead use the `minHeight` and `maxHeight` properties.
     private var heightConstraint: NSLayoutConstraint!
 
+    private var audioWidthCOnstrain: NSLayoutConstraint!
+    
     /// The minimum height constraint to be set
     public var minHeight: CGFloat = 48
 
@@ -99,7 +117,24 @@ open class MSGInputView: UIControl {
         sendButton.isEnabled = false
         sendButton.addTarget(self, action: #selector(sendButtonTapped(_:)), for: .touchUpInside)
     }
-
+    private func setupAudioButton() {
+        audioButton.isEnabled = true
+        let longPress:UILongPressGestureRecognizer = UILongPressGestureRecognizer.init(target: self, action:  #selector(longPress(_:)))
+        longPress.minimumPressDuration = 2
+        audioButton.addGestureRecognizer(longPress)
+    }
+    
+    @objc func longPress(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state == UIGestureRecognizerState.began {
+            messageType = MessageType.AudioRecordStart
+            sendActions(for: .primaryActionTriggered)
+        }
+        else if (gesture.state == UIGestureRecognizerState.ended) {
+            messageType = MessageType.AudioRecordEnd
+            sendActions(for: .primaryActionTriggered)
+        }
+    }
+    
     @objc func sendButtonTapped(_ sender: UIButton) {
         message = textView.text
         textView.text = nil
